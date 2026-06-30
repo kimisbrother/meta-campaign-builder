5fa15de525f9d988078d5068c2b03142908c2b36
김은형 <kimisbrother@wisebirds.com>
2026-06-30 14:45:57 +0900

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 window.APP_CONFIG = {
   // Apps Script를 "웹 앱으로 배포"한 뒤 발급되는 URL을 여기에 붙여넣으세요.
   // 예: https://script.google.com/macros/s/AKfycb.../exec
-  APPS_SCRIPT_URL: 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwGKeHe3HWTGzQa60pWwNrfx-mHE1g4P1erIbjNyU8DkiJYvWHrezO0Zz5jNyhoTUBe/exec',
 
   // 브랜드 목록 (브랜드 선택 카드에 표시됨)
   BRANDS: [
